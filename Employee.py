88877c42e3726273ba622eef74b80d0a0663aa78
Merlin Mathew <mathew.merlinn>
2022-05-23 16:24:44 +0530

    first commit

---
+++ Employee.py
@@ -0,0 +1,15 @@
+class Employee:
+     def _init_(self,eid,ename,elocation,esalary):
+            
+             self.eid =eid
+             self.ename = ename
+             self.elocation = elocation
+             self.esalary = esalary
+    
+     def GetEmployee(self):
+       
+             print(self.eid)
+             print(self.ename)
+             print(self.elocation)
+             print(self.esalary)
+
---
+++ dogclass1.py
@@ -0,0 +1,12 @@
+class Dog:
+ def __init__(self, name, age):
+         self.name = name
+         self.age = age
+
+obj1 = Dog("ozzy",2)
+print(obj1.name)
+print(obj1.age)
+
+obj2 = Dog("shiar",4)
+print(obj2.name)
+print(obj2.age)
